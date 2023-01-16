412428030c077524834b8067c657a3850756a41d
catitgirl <natagan888@gmail.com>
2023-01-16 20:36:16 +0400

    add comment form

---
+++ comm/script.js
@@ -0,0 +1,56 @@
+let comments = [];
+loadComments();
+
+document.getElementById('comment-add').onclick = function(){
+    const commentName = document.getElementById('comment-name');
+    const commentBody = document.getElementById('comment-body');
+    const commentAvatar = document.getElementById('avatar');
+
+    const comment = {
+        name : commentName.value,
+        body : commentBody.value,
+        avatar : commentAvatar.value,
+        time : Math.floor(Date.now() / 1000)
+    }
+
+    commentName.value = '';
+    commentBody.value = '';
+
+    comments.push(comment);
+    saveComments();
+    showComments();
+}
+
+function saveComments(){
+    localStorage.setItem('comments', JSON.stringify(comments));
+}
+
+function loadComments(){
+    if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+    showComments();
+}
+
+function showComments (){
+    const commentField = document.getElementById('comment-field');
+    let out = '';
+    comments.forEach(function(item){
+        out += `<p class="text-right small"><em>${timeConverter(item.time)}</em></p>`;
+        out += `<p class="alert alert-primary" role="alert">${item.name}</p>`;
+        out += `<p class="alert alert-success" role="alert">${item.body}</p>`;
+        out += `<div class="alert alert-image" role="alert">${item.avatar}</div>`;
+    });
+    commentField.innerHTML = out;
+}
+
+function timeConverter(UNIX_timestamp){
+    const a = new Date(UNIX_timestamp * 1000);
+    const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    const year = a.getFullYear();
+    const month = months[a.getMonth()];
+    const date = a.getDate();
+    const hour = a.getHours();
+    const min = a.getMinutes();
+    const sec = a.getSeconds();
+    const time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+    return time;
+  }
